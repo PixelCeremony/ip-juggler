b71d3622d3b0e2f9601d938cfd6c84fd649fe1ce
Martin Pärtel <martin.partel@gmail.com>
2022-08-03 10:34:56 +0300

    Create vlan interface, so it just works on a base Debian installation.

--- src/main.rs
+++ src/main.rs
@@ -23,39 +23,48 @@ use std::{fmt, thread};
 /// Settings
 #[derive(Debug, FromArgs)]
 struct Settings {
-    /// the IP whose owner is to change
+    /// required - the IP whose owner is to change
     #[argh(option)]
     ip_to_juggle: Ipv4Addr,
-    /// gateway of IP to juggle
+    /// required - gateway of IP to juggle
     #[argh(option)]
     gateway: Ipv4Addr,
-    /// netmask of IP to juggle
+    /// required - netmask of IP to juggle
     #[argh(option)]
     netmask: u8,
-    /// local interface to operate on
-    #[argh(option)]
-    local_interface_regex: String,
-    /// total number of participating machines
-    #[argh(option)]
-    total_participants: usize,
-    /// index of this instance in the set of participants
-    #[argh(option)]
-    local_index: usize,
-    /// duration in seconds of holding an address. Default: 15
-    #[argh(option, default = "15.0")]
+
+    /// optional -regex that finds the local base interface. Default: "enp[0-9]*s0"
+    #[argh(option, default = "\"enp[0-9]*s0\".to_string()")]
+    base_interface_regex: String,
+    /// optional - The VLAN number to assign to the VLAN interface. Default: 4000
+    #[argh(option, default = "4000")]
+    vlan: u32,
+    /// optional - The MTU to assign to the VLAN interface. Default: 1400
+    #[argh(option, default = "1400")]
+    mtu: u32,
+
+    /// optional - duration in seconds of holding an address. Default: 30
+    #[argh(option, default = "30.0")]
     turn_duration: f64,
-    /// how long to wait between gratuitous ARP messages while holding the IP. Default: 0.5
+    /// optional - how long to wait between gratuitous ARP messages while holding the IP. Default: 0.5
     #[argh(option, default = "0.5")]
     arp_interval: f64,
-    /// the UDP port for pinging. Default: 1234
+    /// optional - the UDP port for pinging. Default: 1234
     #[argh(option, default = "1234")]
     udp_ping_port: u16,
-    /// interval in seconds for UDP pings. Default: 1
+    /// optional - interval in seconds for UDP pings. Default: 1
     #[argh(option, default = "1.0")]
     udp_ping_interval: f64,
-    /// whether to print arp packets
+    /// optional - whether to print arp packets
     #[argh(option, default = "false")]
     print_arp: bool,
+
+    /// automatic - total number of participating machines. Set automatically by run-remote.rb
+    #[argh(option)]
+    total_participants: usize,
+    /// automatic - index of this instance in the set of participants. Set automatically by run-remote.rb
+    #[argh(option)]
+    local_index: usize,
 }
 
 const ROUTING_TABLE_NUMBER: u32 = 123;
@@ -67,18 +76,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Instance ID: {}", settings.local_index);
     println!("Settings: {:?}", settings);
 
-    let iface: NetworkInterface = {
-        let local_interface_regex = Regex::new(&settings.local_interface_regex)?;
-        let mut all_ifaces = datalink::interfaces();
-        all_ifaces.sort_by_cached_key(|iface| iface.name.clone());
-        all_ifaces
-            .into_iter()
-            .find(|iface| local_interface_regex.is_match(&iface.name))
-            .expect(&format!(
-                "Could not find interface matching regex: {}",
-                settings.local_interface_regex
-            ))
-    };
+    let iface = recreate_vlan_interface(&settings)?;
 
     let (tx, rx) = {
         match datalink::channel(&iface, Default::default()) {
@@ -142,6 +140,60 @@ fn main() -> Result<(), Box<dyn Error>> {
         .expect("ping listener thread panicked");
 
     Ok(())
+}
+
+fn recreate_vlan_interface(settings: &Settings) -> Result<NetworkInterface, SimpleError> {
+    let base_iface: NetworkInterface = {
+        let local_interface_regex = Regex::new(&settings.base_interface_regex)?;
+        let mut all_ifaces = datalink::interfaces();
+        all_ifaces.sort_by_cached_key(|iface| iface.name.clone());
+        all_ifaces
+            .into_iter()
+            .find(|iface| local_interface_regex.is_match(&iface.name))
+            .ok_or(format!(
+                "Could not find interface matching regex: {}",
+                settings.base_interface_regex
+            ))?
+    };
+
+    let vlan_iface_name = format!("{}.{}", base_iface.name, settings.vlan);
+    let _ = run_cmd(&[
+        "ip",
+        "link",
+        "del",
+        &vlan_iface_name,
+    ]);
+    run_cmd(&[
+        "ip",
+        "link",
+        "add",
+        "link",
+        &base_iface.name,
+        "name",
+        &vlan_iface_name,
+        "type",
+        "vlan",
+        "id",
+        &settings.vlan.to_string(),
+    ])?;
+    run_cmd(&[
+        "ip",
+        "link",
+        "set",
+        &vlan_iface_name,
+        "mtu",
+        &settings.mtu.to_string(),
+    ])?;
+    run_cmd(&["ip", "link", "set", "dev", &vlan_iface_name, "up"])?;
+
+    let vlan_iface = datalink::interfaces()
+        .into_iter()
+        .find(|iface| iface.name == vlan_iface_name)
+        .ok_or(format!(
+            "Could not find VLAN interface that I just tried to createD: {}",
+            vlan_iface_name
+        ))?;
+    Ok(vlan_iface)
 }
 
 fn juggler(
@@ -345,14 +397,6 @@ fn give_up_ip(
     Ok(())
 }
 
-fn run_cmd(cmd: &[&str]) -> Result<Vec<u8>, SimpleError> {
-    Command::new(cmd[0])
-        .args(&cmd[1..])
-        .output()
-        .map(|o| o.stdout)
-        .map_err(|e| SimpleError(format!("Failed to run command '{}': {}", cmd.join(" "), e)))
-}
-
 fn ping_sender(interval: f64, dest_ip: Ipv4Addr, port: u16) {
     loop {
         thread::sleep(Duration::from_secs_f64(interval));
@@ -429,6 +473,15 @@ fn handle_incoming_packet(
     Ok(())
 }
 
+fn run_cmd(cmd: &[&str]) -> Result<Vec<u8>, SimpleError> {
+    println!("Running command: {}", cmd.join(" "));
+    Command::new(cmd[0])
+        .args(&cmd[1..])
+        .output()
+        .map(|o| o.stdout)
+        .map_err(|e| SimpleError(format!("Failed to run command '{}': {}", cmd.join(" "), e)))
+}
+
 #[derive(Debug)]
 struct SimpleError(String);
 
@@ -445,8 +498,20 @@ impl From<&str> for SimpleError {
         SimpleError(String::from(s))
     }
 }
+impl From<String> for SimpleError {
+    fn from(s: String) -> Self {
+        SimpleError(s)
+    }
+}
+
 impl From<std::io::Error> for SimpleError {
     fn from(e: std::io::Error) -> Self {
+        SimpleError(e.to_string())
+    }
+}
+
+impl From<regex::Error> for SimpleError {
+    fn from(e: regex::Error) -> Self {
         SimpleError(e.to_string())
     }
 }
